c1672c8a756267531837823660c92430048d7a7a
nibatandukar <nibatandukar@outlook.com>
2023-06-23 10:19:08 +0545

    added vpc:

--- prod/main.tf
+++ prod/main.tf
@@ -1,7 +1,22 @@
-resource "null_resource" "test_prod" {
+provider "aws"  {
+ region = "ap-south-1"
 
 }
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-resource "null_resource" "test_prod2" {
+  name = "my-vpc-1"
+  cidr = "10.0.0.0/16"
 
+  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
